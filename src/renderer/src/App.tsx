bdd87de36830fb2f15dc2a277f165ae3f57f7597
JackDotJS <jackdotjs@proton.me>
2024-07-22 03:35:02 -0700

    update App.tsx

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -4,7 +4,7 @@ import * as i18n from '@solid-primitives/i18n';
 import OptionsWrapper from './components/options/OptionsWrapper';
 
 const App = (): JSXElement => {
-  const { config, dictionary } = useContext(StateContext);
+  const { setState, config, dictionary } = useContext(StateContext);
 
   const t = i18n.translator(() => dictionary(), i18n.resolveTemplate) as Translator;
 
@@ -12,11 +12,10 @@ const App = (): JSXElement => {
     <>
       <OptionsWrapper/>
       <h1>{t(`helloworld`)}</h1>
-      {/* <button onClick={() => setState(`optionsOpen`, true) }>{t(`options.title`)}</button> */}
+      <button onClick={() => setState(`optionsOpen`, true) }>{t(`options.title`)}</button>
       <h1>electron test</h1>
       <button onClick={() => window.electron.pingTest()}>ping</button>
-      <button onClick={() => window.electron.writeConfig({ penis: true })}>writeConfig</button>
-      <button onClick={() => console.debug(config)}>print config</button>
+      <button onClick={() => console.debug(config)}>print config to console</button>
     </>
   );
 };
