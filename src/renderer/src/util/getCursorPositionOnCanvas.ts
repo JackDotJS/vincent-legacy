a9d9acc9603cb5e21cb772685e6e19a84d91f66a
JackDotJS <jackdotjs@proton.me>
2024-08-15 15:04:14 -0700

    frontend: update cursor position util to support webgpu coordinates

--- src/renderer/src/util/getCursorPositionOnCanvas.ts
+++ src/renderer/src/util/getCursorPositionOnCanvas.ts
@@ -1,7 +1,14 @@
 import { state } from "@renderer/state/StateController";
 import getCanvasTransform from "./getCanvasTransform";
 
-const getCursorPositionOnCanvas = (absX: number, absY: number): { x: number, y: number} => {
+type CanvasPositionData = {
+  x: number,
+  y: number,
+  gpuX: number,
+  gpuY: number
+};
+
+const getCursorPositionOnCanvas = (absX: number, absY: number): CanvasPositionData => {
   // we get the rect of the viewport instead of the
   // canvas or its wrapper since rects include css
   // transforms, which is exactly what we dont want.
@@ -38,9 +45,13 @@ const getCursorPositionOnCanvas = (absX: number, absY: number): { x: number, y: 
   x = (x + (canvasWidth / 2)) / state.canvas.scale;
   y = (y + (canvasHeight / 2)) / state.canvas.scale;
 
-  // console.debug(x, y);
+  // webgpu coordinates
+  const gpuX = ((x / state.canvas.main!.width) * 2) - 1;
+  const gpuY = (((y / state.canvas.main!.height) * 2) - 1) * -1;
 
-  return { x, y };
+  // console.debug(x, y, gpuX, gpuY);
+
+  return { x, y, gpuX, gpuY } as CanvasPositionData;
 };
 
 export default getCursorPositionOnCanvas;
