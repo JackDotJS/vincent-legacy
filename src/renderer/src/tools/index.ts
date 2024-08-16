9142637a31071208816f101233a3dcd549ae9e83
JackDotJS <jackdotjs@proton.me>
2024-08-15 18:30:33 -0700

    minor cleanup

--- src/renderer/src/tools/index.ts
+++ src/renderer/src/tools/index.ts
@@ -1,4 +1,3 @@
 export * as PaintbrushTool from './Paintbrush/Paintbrush';
-export * as PaintbrushToolOld from './PaintbrushOLD/PaintbrushOLD';
 export * as Eraser from './Eraser/Eraser';
 export * as BrushSelect from './BrushSelect/BrushSelect';
